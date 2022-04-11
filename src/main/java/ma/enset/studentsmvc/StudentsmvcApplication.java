8c5dfafe831aff73b00f4181261896f5fa062824
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-04-11 09:39:20 +0000

    Initial commit

---
+++ src/main/java/ma/enset/studentsmvc/StudentsmvcApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.studentsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsmvcApplication.class, args);
+	}
+
+}
